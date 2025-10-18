2825229f5d5855c454e4103afa7a35a66775e899
marwa erramla <109670248+marwaerr@users.noreply.github.com>
2025-10-18 14:39:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
